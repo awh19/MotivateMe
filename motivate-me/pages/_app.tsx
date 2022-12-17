bf5a77e6d85eb2c4a6679728cf744ec638f3f387
Andrew Hill <andrew.hill.w@gmail.com>
2022-12-16 19:45:55 -0500

    Toggle Dark Mode, Adding Heading

--- motivate-me/pages/_app.tsx
+++ motivate-me/pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <ChakraProvider>
+      <ColorModeScript initialColorMode='light'></ColorModeScript>
       <Component {...pageProps} />
     </ChakraProvider>
   )
