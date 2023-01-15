5bbe47456cafe14d54a64230a1e003bcc6099222
Andrew Hill <andrew.hill.w@gmail.com>
2023-01-15 18:40:38 -0500

    Adding Video Iframe, Random Generation Algorithm

--- motivate-me/src/Pages/videos.tsx
+++ motivate-me/src/Pages/videos.tsx
@@ -1,19 +1,37 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import React, { useState } from 'react'
+import { Button, AspectRatio, VStack } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import NavBar from '../Components/NavBar'
 
-export default function Video() {
+
+const videoUrls = [
+    "https://www.youtube.com/embed/ERClHCOF14c",
+    "https://www.youtube.com/embed/tgbNymZ7vqY",
+    "https://www.youtube.com/embed/ktvTqknDobU"
+]
+
+export default function Videos() {
+    const [url, setUrl] = useState(videoUrls[0])
+
+    const handleClick = () => {
+        const randomIndex = Math.floor(Math.random() * videoUrls.length)
+        setUrl(videoUrls[randomIndex])
+    }
 
     return (
-        <><NavBar /><VStack p={4}>
-            <Heading
-                mb="8"
-                fontWeight="extrabold"
-                size="2xl"
-                bgColor="teal.300"
-                bgClip="text">
-                Video Player
-            </Heading>
-        </VStack></>
-
+        <>
+            <NavBar />
+            <VStack p={4}>
+                <AspectRatio width="50%" ratio={16 / 9}>
+                    <Box
+                        as="iframe"
+                        title="Random Video"
+                        src={url}
+                        allowFullScreen
+                    ></Box>
+                </AspectRatio>
+                <Button onClick={handleClick}>Generate Random Video</Button>
+            </VStack>
+        </>
     )
 }
